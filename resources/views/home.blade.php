a7643386003a4cb0da96c9cebcd3fa9365c02c48
MrElle <cassaralavoro@gmail.com>
2024-01-23 09:54:59 +0100

    add style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,16 +4,16 @@
     <div class="container">
         <div class="row">
             @foreach($movies as $movie)
-                <div class="col-2">
-                    <div class="card">
+                <div class="col-3">
+                    <div class="card mt-3">
                         <div class="card-body">
                             <div class="card-title">
-                                <h6>{{ $movie->original_title }}</h6>
+                                <h6 class="text-center fw-bold">{{ $movie->original_title }}</h6>
                             </div>
                             <div class="card-subtitle">
-                                <p class="text-end">Nazionalita': {{ $movie->nationality }}</p>
-                                <p class="text-end">Data di uscita: {{ $movie->date }}</p>
-                                <p class="text-end">Voto: {{ $movie->vote }}</p>
+                                <p class="mt-3 fst-italic mb-0">Nazionalita': {{ $movie->nationality }}</p>
+                                <p class="fst-italic">Data di uscita: {{ $movie->date }}</p>
+                                <p class="text-end fst-italic m-0">Voto: {{ $movie->vote }}</p>
                             </div>
                         </div>
                     </div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,3 @@
 <header>
-    <h1 class="text-center fw-bold pt-3">Lista film</h1>
+    <h1 class="text-center fw-bold pt-3 text-uppercase">Lista film</h1>
 </header>
